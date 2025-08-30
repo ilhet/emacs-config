7d0be385909613f5333f42c5efd01cc7b2aa26db
iqwuegf <87618413+iqwuegf@users.noreply.github.com>
2025-08-30 22:31:40 +0200

    This is the initial commit.

---
+++ init.el
@@ -0,0 +1,54 @@
+;; disable startup message
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)    ;disable scroll bar
+(tool-bar-mode -1)      ;disable tool bar
+(tooltip-mode -1)       ;disable tooltip
+(menu-bar-mode -1)      ;disable menu bar
+
+(load-theme 'tango-dark)     ;set theme
+
+(transient-mark-mode 1)
+
+;; initialize package sources
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/Documents/Notizen/1.org"))
+ '(package-selected-packages '(auctex org-roam yasnippet)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;auctex customization
+(use-package auctex)
+(setq org-latex-create-formula-image-program 'dvipng)
+
+(use-package yasnippet)
+
+;;org-roam
+(use-package org-roam)
+(setq org-roam-directory (file-truename "~/Documents/Notizen"))
+(org-roam-db-autosync-mode)
+
+
